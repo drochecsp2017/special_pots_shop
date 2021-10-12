f916ada06cea586d7cde5faf8f5f59ef45ac65b6
David Roche <droche711@gmail.com>
2021-10-12 16:49:56 -0400

    Change in tact
    I did the math, and I don't think we actually need arbitrary precision
    number types. Long should be sufficient, and likely a fair bit faster.

--- special_shop.kt
+++ special_shop.kt
@@ -1,34 +1,30 @@
 
-//import kotlin.io.readline;
-import java.math.BigInteger;
-import java.math.BigDecimal;
-import java.math.MathContext;
 
 
-class Parabola(potCount: BigInteger, xMult: BigInteger, yMult: BigInteger) {
+
+class Parabola(potCount: Long, xMult: Long, yMult: Long) {
     private val coeffA = xMult + yMult;
-    private val coeffB = BigInteger.valueOf(-2) * yMult * potCount;
-    private val coeffC = potCount.pow(2) * yMult;
-    private val roundingMode = MathContext(0);
+    private val coeffB = -2 * yMult * potCount;
+    private val coeffC = potCount * potCount * yMult;
 
-    fun minXCoord(): BigDecimal {
-        val numerator = BigDecimal(-coeffB);
-        val denominator = BigDecimal(coeffA * BigInteger.valueOf(2));
-        val result = numerator.divide(denominator, roundingMode.getRoundingMode());
-        return result.round(roundingMode);
+    fun minXCoord(): Long {
+        val numerator = -1.0 * coeffB;
+        val denominator = coeffA * 2.0;
+        val result = numerator / denominator;
+        return result.toLong();
     }
 
-    fun solveAt(xCoord: BigDecimal): BigDecimal {
-        val termA = xCoord.pow(2) * BigDecimal(coeffA)
-        val termB = xCoord * BigDecimal(coeffB)
-        val termC = BigDecimal(coeffC)
-        val result = termA + termB + termC;
-        return result.round(roundingMode);
+    fun solveAt(xCoord: Double): Long {
+        val termA = xCoord * xCoord * coeffA;
+        val termB = xCoord * coeffB;
+        val termC = coeffC.doubleValue();
+        val realResult = termA + termB + termC;
+        return realResult.toLong();
     }
 }
 
 
-fun solveCase(potCount: BigInteger, xMult: BigInteger, yMult: BigInteger): BigInteger {
+fun solveCase(potCount: Long, xMult: Long, yMult: Long): Long {
     val eq = Parabola(potCount, xMult, yMult);
     val optimal_a_pots = eq.minXCoord();
     val soln = eq.solveAt(optimal_a_pots);
@@ -37,9 +33,9 @@ fun solveCase(potCount: BigInteger, xMult: BigInteger, yMult: BigInteger): BigIn
 
 fun parseTestCase(): Unit {
     val caseParams = readLine()!!.split(" ");
-    val potsToBuy = BigInteger(caseParams[0]);
-    val xCost = BigInteger(caseParams[1]);
-    val yCost = BigInteger(caseParams[2]);
+    val potsToBuy = caseParams[0].toLong();
+    val xCost = caseParams[1].toLong();
+    val yCost = caseParams[2].toLong();
     val result = solveCase(potsToBuy, xCost, yCost);
     println(result);
 }
