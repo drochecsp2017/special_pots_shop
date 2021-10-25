0e6a6cf01e878a632b512363880d0b02d1e8f57f
David Roche <droche711@gmail.com>
2021-10-25 12:19:15 -0400

    Corrected Rust warnings
    Apparently snake case is firectl requested by the compiler...

--- special_shop.rs
+++ special_shop.rs
@@ -14,60 +14,60 @@ struct Equation {
 
 impl Equation {
 
-    fn calcEquation(args: Input) -> Equation {
-        let numPots = args.0 as Float;
-        let aMult = args.1 as Float;
-        let bMult = args.2 as Float;
+    fn calc_equation(args: Input) -> Equation {
+        let num_pots = args.0 as Float;
+        let a_mult = args.1 as Float;
+        let b_mult = args.2 as Float;
         return Equation {
-            coeff_a: aMult as Float + bMult,
-            coeff_b: -2.0 * (bMult * numPots) as Float,
-            coeff_c: bMult * numPots.powi(2) as Float,
+            coeff_a: a_mult + b_mult,
+            coeff_b: -2.0 * (b_mult * num_pots),
+            coeff_c: b_mult * num_pots.powi(2),
         };
     }
 
-    fn findEqMin(&self) -> Float {
+    fn find_eq_min(&self) -> Float {
         let numerator = -1.0 * self.coeff_b;
         let denominator = 2.0 * self.coeff_a;
         let result = numerator / denominator;
         return result;
     }
 
-    fn solveAt(&self, x: Float) -> Float {
-        let termA = self.coeff_a.powi(2) * x;
-        let termB = self.coeff_b * x;
-        let termC = self.coeff_c;
-        return termA + termB + termC;
+    fn solve_at(&self, x: Float) -> Float {
+        let term_a = self.coeff_a.powi(2) * x;
+        let term_b = self.coeff_b * x;
+        let term_c = self.coeff_c;
+        return term_a + term_b + term_c;
     }
 
 } // impl Equation
 
-fn solveCase(args: Input) -> Integer {
-    let eq = Equation::calcEquation(args);
-    let min = eq.findEqMin();
-    let soln = eq.solveAt(min.round());
+fn solve_case(args: Input) -> Integer {
+    let eq = Equation::calc_equation(args);
+    let min = eq.find_eq_min();
+    let soln = eq.solve_at(min.round());
     return soln.round() as Integer;
 }
 
-fn parseCase(buffer: &mut String) -> Option<Input> {
+fn parse_case(buffer: &mut String) -> Option<Input> {
     match io::stdin().read_line(buffer) {
         Ok(_n) => {
             let trimmed_buffer = buffer.trim();
             let vec: Vec<&str> = trimmed_buffer.split(" ").collect();
     
-            let numPots = vec[0].parse::<Integer>().unwrap();
-            let aMult = vec[1].parse::<Integer>().unwrap();
-            let bMult = vec[2].parse::<Integer>().unwrap();
-            return Some((numPots, aMult, bMult))
+            let num_pots = vec[0].parse::<Integer>().unwrap();
+            let a_mult = vec[1].parse::<Integer>().unwrap();
+            let b_mult = vec[2].parse::<Integer>().unwrap();
+            return Some((num_pots, a_mult, b_mult))
         }
 
         Err(_error) => return None,
     }
 }
 
-fn runCase(buffer: &mut String) {
-    match parseCase(buffer) {
+fn run_case(buffer: &mut String) {
+    match parse_case(buffer) {
         Some(args) => {
-            let soln = solveCase(args);
+            let soln = solve_case(args);
             println!("{}", soln);
         }
 
@@ -77,7 +77,7 @@ fn runCase(buffer: &mut String) {
 }
 
 
-fn getNumCases(buffer: &mut String) -> i32 {
+fn get_num_cases(buffer: &mut String) -> i32 {
     io::stdin().read_line(buffer).expect("could not read line");
     let num_cases = buffer.trim().parse::<i32>().unwrap();
     buffer.clear();
@@ -86,10 +86,10 @@ fn getNumCases(buffer: &mut String) -> i32 {
 
 fn main() {
     let mut buffer = String::new();
-    let numCases = getNumCases(&mut buffer);
+    let num_cases = get_num_cases(&mut buffer);
 
-    for _ in 0..numCases {
-        runCase(&mut buffer);
+    for _ in 0..num_cases {
+        run_case(&mut buffer);
     }
 }
 
